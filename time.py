af83ce56950f016208de750f0f0b74b843fcd00f
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-02-08 09:12:54 -0500

    Create time.py

---
+++ time.py
@@ -0,0 +1,8 @@
+#Import the time module
+import time
+
+#The only command I know is this:
+
+time.sleep(1)
+
+#You can change the 1 to any number you'd like. It's just how many seconds you want to wait.
